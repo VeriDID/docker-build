1a21e0dc6c9f4c86171f13d133fae2860f008d0a
Elanie Quiambao <elanietquiambao@gmail.com>
2022-10-31 23:35:49 -0400

    updated postgres cs

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,22 +1,17 @@
 import { Injectable } from "@nestjs/common";
 import { Client } from "pg";
 
-// function postgresCS() {
-//   const postgres_user = process.env.POSTGRES_USER;
-//   const postgres_pw = process.env.POSTGRES_PASSWORD;
-//   const postgres_host = process.env.POSTGRES_HOST;
-//   const postgres_db = process.env.POSTGRES_DBNAME;
+const postgres_user = process.env.POSTGRES_USER;
+const postgres_pw = process.env.POSTGRES_PASSWORD;
+const postgres_host = process.env.POSTGRES_HOST;
 
-//   if (postgres_user === undefined || postgres_pw === undefined) {
-//     console.log("Undefined postgres credentials");
-//     return null;
-//   }
+console.log("user: " + postgres_user);
+console.log("pw: " + postgres_pw);
+console.log("host: " + postgres_host);
 
-//   return `postgres://${postgres_user}:${postgres_pw}@${postgres_host}/${postgres_db}`;
-// }
+const cs = `postgres://${postgres_user}:${postgres_pw}@${postgres_host}/postgres`;
 
-const cs = "postgres://postgresUser:postgresPW@10.109.20.239:5432/postgres";
-// const cs = postgresCS();
+// const cs = "postgres://postgresUser:postgresPW@10.109.20.239:5432/postgres";
 
 // docker run --name myPostgresDb -p 5455:5432 -e POSTGRES_USER=postgresUser -e POSTGRES_PASSWORD=postgresPW -e POSTGRES_DB=postgresDB -d postgres
 
